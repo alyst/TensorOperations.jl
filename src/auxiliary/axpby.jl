7711e89699398d1776065ea988f28f56c06ceb7c
Alexey Stukalov <astukalov@gmail.com>
2016-05-25 16:36:21 +0200

    replace _one/_zero with Val{0}/Val{1}

--- src/auxiliary/axpby.jl
+++ src/auxiliary/axpby.jl
@@ -1,25 +1,17 @@
 # auxiliary/axpby.jl
 #
-# Simple wrapper for the operation of computing α * x + β * y, and two
-# special singleton types (immutables) to remove any overhead of multiplication
+# Simple wrapper for the operation of computing α * x + β * y
+# to remove any overhead of multiplication
 # by one or addition by zero.
 
-immutable Zero <: Integer
-end
-immutable One <: Integer
-end
+axpby(α::Type{Val{1}}, x, β::Type{Val{1}}, y) = x+y
+axpby(α::Type{Val{0}}, x, β::Type{Val{1}}, y) = y
+axpby(α::Type{Val{1}}, x, β::Type{Val{0}}, y) = x
+axpby(α::Type{Val{0}}, x, β::Type{Val{0}}, y) = zero(y)
 
-const _zero = Zero()
-const _one = One()
+axpby(α::Type{Val{1}}, x, β,               y) = x+β*y
+axpby(α::Type{Val{0}}, x, β,               y) = β*y
+axpby(α,               x, β::Type{Val{0}}, y) = α*x
+axpby(α,               x, β::Type{Val{1}}, y) = α*x+y
 
-axpby(α::One,  x, β::One,  y) = x+y
-axpby(α::Zero, x, β::One,  y) = y
-axpby(α::One,  x, β::Zero, y) = x
-axpby(α::Zero, x, β::Zero, y) = zero(y)
-
-axpby(α::One,  x, β,       y) = x+β*y
-axpby(α::Zero, x, β,       y) = β*y
-axpby(α,       x, β::Zero, y) = α*x
-axpby(α,       x, β::One,  y) = α*x+y
-
-axpby(α,       x, β,       y) = α*x+β*y
+axpby(α,               x, β,               y) = α*x+β*y
